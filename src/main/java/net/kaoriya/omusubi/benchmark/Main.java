3227b90d8423ea04f2d484f15254fc0ff8159574
MURAOKA Taro <koron.kaoriya@gmail.com>
2014-01-29 14:43:44 +0900

    update benchmark results

--- src/main/java/net/kaoriya/omusubi/benchmark/Main.java
+++ src/main/java/net/kaoriya/omusubi/benchmark/Main.java
@@ -1,7 +1,6 @@
 package net.kaoriya.omusubi.benchmark;
 
 import me.lemire.integercompression.BinaryPacking;
-import me.lemire.integercompression.Composition;
 import me.lemire.integercompression.DeltaZigzagBinaryPacking2;
 import me.lemire.integercompression.IntegratedBinaryPacking;
 import me.lemire.integercompression.VariableByte;
@@ -12,10 +11,8 @@ public class Main {
         b.prepare();
         b.execute(new OmusubiCodec());
         b.execute(new LemireCodec(new DeltaZigzagBinaryPacking2()));
+        b.execute(new LemireCodec(new BinaryPacking()));
         b.execute(new LemireCodec(new IntegratedBinaryPacking()));
-        b.execute(new LemireCodec(new Composition(
-                        new BinaryPacking(),
-                        new VariableByte())));
         b.report(System.out);
     }
 }
