15a9e59a00201f4123a70ece6d4b78ce939e9b2c
MURAOKA Taro <koron.kaoriya@gmail.com>
2014-02-05 13:39:45 +0900

    add JustCopy benchmark, and remove IntegratedBinaryPack

--- src/main/java/net/kaoriya/omusubi/benchmark/Main.java
+++ src/main/java/net/kaoriya/omusubi/benchmark/Main.java
@@ -2,7 +2,7 @@ package net.kaoriya.omusubi.benchmark;
 
 import me.lemire.integercompression.BinaryPacking;
 import me.lemire.integercompression.DeltaZigzagBinaryPacking2;
-import me.lemire.integercompression.IntegratedBinaryPacking;
+import me.lemire.integercompression.JustCopy;
 import me.lemire.integercompression.VariableByte;
 
 public class Main {
@@ -12,7 +12,7 @@ public class Main {
         b.execute(new OmusubiCodec());
         b.execute(new LemireCodec(new DeltaZigzagBinaryPacking2()));
         b.execute(new LemireCodec(new BinaryPacking()));
-        b.execute(new LemireCodec(new IntegratedBinaryPacking()));
+        b.execute(new LemireCodec(new JustCopy()));
         b.report(System.out);
     }
 }
