45026e662cfa6e36ec53ae77fedefd3b873b0b39
MURAOKA Taro <koron.kaoriya@gmail.com>
2014-03-06 00:32:20 +0900

    more benchmarks for comparison

--- src/main/java/net/kaoriya/omusubi/benchmark/Main.java
+++ src/main/java/net/kaoriya/omusubi/benchmark/Main.java
@@ -4,26 +4,36 @@ import me.lemire.integercompression.BinaryPacking;
 import me.lemire.integercompression.DeltaZigzagBinaryPacking2;
 import me.lemire.integercompression.JustCopy;
 import me.lemire.integercompression.VariableByte;
+import net.kaoriya.omusubi.IntBitPacking;
 import net.kaoriya.omusubi.IntDZBP;
 import net.kaoriya.omusubi.IntJustCopy;
+import net.kaoriya.omusubi.LongBitPacking;
 import net.kaoriya.omusubi.LongDZBP;
 import net.kaoriya.omusubi.LongJustCopy;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("");
-        System.gc();
-        benchmarkInt();
-        System.out.println("");
-        System.gc();
+        benchmarkIntOmusubi();
+        benchmarkIntLemire();
         benchmarkLong();
     }
 
-    public static void benchmarkInt() {
+    public static void benchmarkIntOmusubi() {
+        System.out.println("");
+        System.gc();
         Benchmarks b = new Benchmarks();
         b.prepare();
         b.execute(new OmusubiCodec(new IntDZBP()));
+        b.execute(new OmusubiCodec(new IntBitPacking()));
         b.execute(new OmusubiCodec(new IntJustCopy()));
+        b.report(System.out);
+    }
+
+    public static void benchmarkIntLemire() {
+        System.out.println("");
+        System.gc();
+        Benchmarks b = new Benchmarks();
+        b.prepare();
         b.execute(new LemireCodec(new DeltaZigzagBinaryPacking2()));
         b.execute(new LemireCodec(new BinaryPacking()));
         b.execute(new LemireCodec(new JustCopy()));
@@ -31,9 +41,12 @@ public class Main {
     }
 
     public static void benchmarkLong() {
+        System.out.println("");
+        System.gc();
         LongBenchmarks b = new LongBenchmarks();
         b.prepare();
         b.execute(new LongOmusubiCodec(new LongDZBP()));
+        b.execute(new LongOmusubiCodec(new LongBitPacking()));
         b.execute(new LongOmusubiCodec(new LongJustCopy()));
         b.report(System.out);
     }
