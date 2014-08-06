db52ebc6619b795e50eeed43c005e058b3fc4d03
MURAOKA Taro <koron.kaoriya@gmail.com>
2014-08-06 12:57:08 +0900

    define criteria of benchmarks

--- src/main/java/net/kaoriya/omusubi/benchmark/Main2.java
+++ src/main/java/net/kaoriya/omusubi/benchmark/Main2.java
@@ -1,7 +1,11 @@
 package net.kaoriya.omusubi.benchmark;
 
+import java.util.List;
+
+import net.kaoriya.omusubi.IntArrayInputStream;
 import net.kaoriya.omusubi.IntArrayOutputStream;
 import net.kaoriya.omusubi.IntAscSDBP;
+import java.util.ArrayList;
 
 public class Main2 {
 
@@ -14,8 +18,11 @@ public class Main2 {
 
     public static void benchmark() {
         benchmarkUnion();
+        benchmarkUnionRaw();
         benchmarkIntersect();
+        benchmarkIntersectRaw();
         benchmarkDifference();
+        benchmarkDifferenceRaw();
     }
 
     public static void run(String name, Runnable proc) {
@@ -28,9 +35,20 @@ public class Main2 {
     public static void benchmarkUnion() {
         final byte[] mul2 = packedIntSet(0, 2, NUM_MAX);
         final byte[] mul3 = packedIntSet(0, 3, NUM_MAX);
-        run("union", new Runnable() {
+        run("union (packed)", new Runnable() {
             public void run() {
                 byte[] result = IntAscSDBP.union(mul2, mul3);
+            }
+        });
+    }
+
+    public static void benchmarkUnionRaw() {
+        final int[] mul2 = generateIntSet(0, 2, NUM_MAX);
+        final int[] mul3 = generateIntSet(0, 3, NUM_MAX);
+        run("union    (raw)", new Runnable() {
+            public void run() {
+                List<IntAscSDBP.Reader> readers = newReaders(mul2, mul3);
+                int[] result = IntAscSDBP.union(readers).toIntArray();
             }
         });
     }
@@ -38,9 +56,22 @@ public class Main2 {
     public static void benchmarkIntersect() {
         final byte[] mul2 = packedIntSet(0, 2, NUM_MAX);
         final byte[] mul3 = packedIntSet(0, 3, NUM_MAX);
-        run("intersect", new Runnable() {
+        run("intersect (packed)", new Runnable() {
             public void run() {
                 byte[] result = IntAscSDBP.intersect(mul2, mul3);
+            }
+        });
+    }
+
+    public static void benchmarkIntersectRaw() {
+        final int[] mul2 = generateIntSet(0, 2, NUM_MAX);
+        final int[] mul3 = generateIntSet(0, 3, NUM_MAX);
+        run("intersect    (raw)", new Runnable() {
+            public void run() {
+                IntAscSDBP.Reader pivot = newReader(mul2);
+                List<IntAscSDBP.Reader> readers = newReaders(mul3);
+                int[] result =
+                    IntAscSDBP.intersect(pivot, readers).toIntArray();
             }
         });
     }
@@ -48,9 +79,22 @@ public class Main2 {
     public static void benchmarkDifference() {
         final byte[] mul2 = packedIntSet(0, 2, NUM_MAX);
         final byte[] mul3 = packedIntSet(0, 3, NUM_MAX);
-        run("difference", new Runnable() {
+        run("difference (packed)", new Runnable() {
             public void run() {
                 byte[] result = IntAscSDBP.difference(mul2, mul3);
+            }
+        });
+    }
+
+    public static void benchmarkDifferenceRaw() {
+        final int[] mul2 = generateIntSet(0, 2, NUM_MAX);
+        final int[] mul3 = generateIntSet(0, 3, NUM_MAX);
+        run("difference    (raw)", new Runnable() {
+            public void run() {
+                IntAscSDBP.Reader pivot = newReader(mul2);
+                List<IntAscSDBP.Reader> readers = newReaders(mul3);
+                int[] result =
+                    IntAscSDBP.difference(pivot, readers).toIntArray();
             }
         });
     }
@@ -65,5 +109,21 @@ public class Main2 {
 
     static byte[] packedIntSet(int start, int offset, int max) {
         return IntAscSDBP.toBytes(generateIntSet(start, offset, max));
+    }
+
+    static IntAscSDBP.Reader newReader(int[] data) {
+        IntAscSDBP.Reader r =
+            new IntAscSDBP.Reader(new IntArrayInputStream(data));
+        r.read();
+        return r;
+    }
+
+    static List<IntAscSDBP.Reader> newReaders(int[] ...data) {
+        ArrayList<IntAscSDBP.Reader> readers =
+            new ArrayList<IntAscSDBP.Reader>(data.length);
+        for (int[] d : data) {
+            readers.add(newReader(d));
+        }
+        return readers;
     }
 }
